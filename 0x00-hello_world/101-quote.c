17b249fab4253d3780cb9a678893659d89a7a4bb
Israel025 <Israeltademola@gmail.com>
2022-04-08 11:02:30 +0000

    modified the 101-quote.c C program file +++

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	
-	write(STDERR_FILENO, st, sizeof(st));
+	write(STDERR_2, st, sizeof(st));
 	return (1);
 }
