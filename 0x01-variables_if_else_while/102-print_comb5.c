e136b4545ac34bc3d1b19ae6550566a57b97f0cd
Israel025 <Israeltademola@gmail.com>
2022-04-10 17:17:46 +0000

    modified 102-print_comb5.c program file+++

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,39 +11,40 @@
 */
 int main(void)
 {
-	int h, k, i, j;
+int h, k, i, j;
 
-	for (h = 48; h < 58; h++)
+for (h = 48; h < 58; h++)
+{
+	for (k = 48; k < 58; k++)
 	{
-		for (k = 48; k < 58; k++)
+		for (i = 48; i < 58; i++)
 		{
-			for (i = 48; i < 58; i++)
+			for (j = 48; j < 58; j++)
 			{
-				for(j = 48; j < 58; j++)
+				int x = (h * 10) + k;
+				int y = (i * 10) + j;
+
+				if (x < y)
 				{
-					int x = (h * 10) + k;
-					int y = (i * 10) + j;
-					if (x < y)
+					putchar(h);
+					putchar(k);
+					putchar(' ');
+					putchar(i);
+					putchar(j);
+					if ((h == 57) && (k == 56) && (i == 57) && (j == 57))
 					{
-						putchar(h);
-						putchar(k);
+						continue;
+					}
+					else
+					{
+						putchar(',');
 						putchar(' ');
-						putchar(i);
-						putchar(j);
-						if ((h == 57) && (k == 56) && (i == 57) && (j == 57))
-						{
-							continue;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
 					}
 				}
 			}
 		}
 	}
+}
 	putchar('\n');
 	return (0);
 }
