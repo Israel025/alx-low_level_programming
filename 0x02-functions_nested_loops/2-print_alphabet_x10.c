5b6dea349f2d5dce7d7e6f7cc4b1490a3f25026c
Israel025 <Israeltademola@gmail.com>
2022-04-12 17:20:43 +0000

    created the 2-print_alphabet_x10.c program file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in small caps 10x
+ *
+ * Description: This function prints out all of the letters
+ * of the alphabet in small caps, on ten different lines followed
+ * by new line. its a void function, does not return anything
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+	       	_putchar('\n');
+	}
+}
