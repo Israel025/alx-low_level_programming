014f4df4dd8cee42229c1dcfbd21f76e07fec8f7
Israel025 <Israeltademola@gmail.com>
2022-04-12 16:19:28 +0000

    modified the 1-alphabet.c program file Xx+

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,6 @@
  *Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-/* prints the alphabet, in lowercase followed by new line */
 void print_alphabet(void)
 {
 	/* prints the alphabet, in lowercase followed by new line */
