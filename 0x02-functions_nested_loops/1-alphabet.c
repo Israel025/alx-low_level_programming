e4ce66e526c05bda02710e653fd0ce4a7e6454ae
Israel025 <Israeltademola@gmail.com>
2022-04-12 16:10:09 +0000

    modified the 1-alphabet.c program file Xix+

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
-/**
- *prints the alphabet, in lowercase
- *followed by new line
- */
+//prints the alphabet, in lowercase followed by new line
+
 
 
 /*
  *Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
+//prints the alphabet, in lowercase followed by new line
 void print_alphabet(void)
 {
+	//prints the alphabet, in lowercase followed by new line
 	int i;
 
 	for (i = 97; i < 123; i++)
