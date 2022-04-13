c36ebff9fa864e98ef4547a5fb630db2c330edea
Israel025 <Israeltademola@gmail.com>
2022-04-13 05:16:33 +0000

    created the 11-print_to_98.c program file 6+

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,37 +11,37 @@
 void print_to_98(int n)
 {
 	if (n > 98)
-        {
-                for (n = n; n >= 98; n--)
-                {
-                        if (n == 98)
-                        {
-                                printf("%d", n);
-                        }
-                        else
-                        {
-                                printf("%d, ", n);
-                        }
-                }
-                putchar('\n');
-        }
-        else if (n < 98)
-        {
-                for (n = n; n <= 98; n++)
-                {
-                        if (n == 98)
-                        {
-                                printf("%d", n);
-                        }
-                        else
-                        {
-                                printf("%d, ", n);
-                        }
-                }
-                putchar('\n');
-        }
-        else
-        {
-                printf("%d\n", n);
-        }
+	{
+		for (n = n; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+		putchar('\n');
+	}
+	else if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+		putchar('\n');
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
 }
