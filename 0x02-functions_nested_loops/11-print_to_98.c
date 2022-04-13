8b887abf79ea5de19a4368da68e98ff7ee9f6aea
Israel025 <Israeltademola@gmail.com>
2022-04-13 04:47:10 +0000

    created the 11-print_to_98.c program file 4+

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,39 +8,19 @@
  * number to 98 (either backwards or forward).
  * Return: this is a void function no value(s) returned
  */
-void print_to_98(int n){
-	if (n > 98)
-	{
-		for (n = n; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-		putchar('\n');
-	}
-	else if (n < 98)
-	{
-		for (n = n; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-		putchar('\n');
-	}
-	else
-	{
-		printf("%d\n", n);
-	}
+
+void print_to_98(int n)
+{
+if (n < 98)
+{
+for (n = n; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
+else
+{
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
 }
