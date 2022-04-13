10460e03885d38dfc715c21cc9d8b5ea6143e79c
Israel025 <Israeltademola@gmail.com>
2022-04-13 00:20:21 +0000

    created the 8-24_hours.c program file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - every minutes of the day.
+ *
+ * Description: This function printes every minutes
+ * of the day starting from 00:00 to 23:59.
+ * Return: this is a void function no value(s) returned
+ */
+void jack_bauer(void)
+{
+int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar (i / 10 + '0');
+			_putchar (i % 10 + '0');
+			_putchar (':');
+			_putchar (j / 10 + '0');
+			_putchar (j % 10 + '0');
+			_putchar ('\n');
+		}
+	}	
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -13,3 +13,7 @@
 ** The 6-abs.c program pcomputes the absolute value of an integer.
 
 ** The 7-print_last_digit.c program prints the last digit of a number.
+
+** The 8-24_hours.c program prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+
+
