0f3ed1c801ae52f7fa2bde50fca9842b47dd1f2e
Israel025 <Israeltademola@gmail.com>
2022-04-14 18:10:05 +0000

    created the 5-more_numbers.c function program file 9+

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,25 +9,22 @@
  */
 void more_numbers(void)
 {
-	int i, j;
-	int hold1 = 0;
-	int hold2 = 0;
+	int i, j, hold1, hold2;
 
 	for (i = 0; i < 10; i++)
 	{
 		for(j = 0; j < 15; j++)
 		{
 			hold1 = j;
+
 			if (j > 9)
 			{
 				hold1 = j / 10;
 				hold2 = j % 10;
 			}
-
 			_putchar(hold1 + '0');
 			if (j > 9)
 				_putchar(hold2 + '0');
-
 		}
 		_putchar('\n');
 	}
