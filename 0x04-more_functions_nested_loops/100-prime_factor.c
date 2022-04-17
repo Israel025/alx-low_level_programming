31bd0638f4035131fbe4b61498edd5e274a3eee7
Israel025 <Israeltademola@gmail.com>
2022-04-17 19:17:12 +0000

    created the 100-prime_factor.c program file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the highest prime factor
+ * of the number 612852475143.
+ *
+ * Description: does as explained above ending
+ * with a new line.
+ * Return: void,  no return value.
+ */
+int main(void)
+{
+	int i = 2;
+	long num = 612852475143;
+
+	while (num > 1)
+	{
+		if (num % i == 0)
+			num /= i;
+		else
+			i++;
+	}
+	printf("%lu\n", i);
+	return (0);
+}
