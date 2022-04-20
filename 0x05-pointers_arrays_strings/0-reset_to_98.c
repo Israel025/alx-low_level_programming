87dd780aeca51cb1c235c4b77f13bdeb460520b5
Israel025 <Israeltademola@gmail.com>
2022-04-20 12:24:19 +0000

    Created priject directory and first tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets an int variable to 98 using pointer.
+ * @*n: The pointer variable
+ *
+ * Description: functions as described above
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
