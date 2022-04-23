6893188aef8508b65b635c7f08d35b27199e1fd8
Israel025 <Israeltademola@gmail.com>
2022-04-23 11:41:57 +0000

    Created the 3-strcmp.c function program file+++

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,51 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings.
- * @s1: First string to be compared.
- * @s2: Second string to be compared.
+ * _strlen - gets the absolute lenght of a string.
+ * * @k: string to count
  *
  * Description: functions as described above
- * Return: a pointer to the resulting string dest.
+ * Return: the count value.
+ */
+int _strlen(char *k)
+{
+	int count, itr;
+
+	count = 0;
+	for (itr = 0; k[itr] != '\0'; itr++)
+		count++;
+	return (count);
+}
+
+/**
+ * _strcmp - compares two strings with each other
+ * for equality.
+ *
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Description: functions as described above
+ * Return: an integer value (signed/unsigned).
  */
 int _strcmp(char *s1, char *s2)
 {
-	int j, cmp;
+	int j, cmp, len_s1, len_s2, longest;
 
 	j = 0;
 	cmp = 0;
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
 
-	while (s1[j] || s2[j])
+	if (len_s1 > len_s2)
+		longest = len_s1;
+	else if (len_s1 < len_s2)
+		longest = len_s2;
+	else if (len_s1 == len_s2)
+		longest = len_s1;
+
+	while (j <= longest)
 	{
-		if (s1[j] == s2[j])
-		{
-			continue;
-		}
-		else if (s1[j] > s2[j])
+		if (s1[j] > s2[j])
 		{
 			cmp = s1[j] - s2[j];
 			break;
