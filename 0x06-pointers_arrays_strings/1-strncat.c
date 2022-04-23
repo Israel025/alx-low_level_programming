6ee3c05eaf52f69c76f4116dcaa452672b09bc73
Israel025 <Israeltademola@gmail.com>
2022-04-23 00:54:00 +0000

    Created the 1-strncat.c function program file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * _strlen - gets the absolute lenght of a string.
+ * * @k: string to count
+ *
+ * Description: functions as described above
+ * Return: the count value.
+ */
+int _strlen(char *k)
+{
+	int count, itr;
+
+	count = 0;
+	for (itr = 0; n[itr] != '\0'; itr++)
+		count++;
+	return (count);
+}
+
+/**
+ * _strncat - concatentes two strings up to
+ * n specified characters from source.
+ *
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: amount of characters to be copied.
+ *
+ * Description: functions as described above
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, dest_nxt, src_len;
+
+	dest_nxt = _strlen(dest);
+	src_len = _strlen(src);
+
+	if (n >= 0 && n <= src_len)
+	{
+		for (i = 0; i < n; i++)
+		{
+			dest[dest_nxt] = src[i];
+			dest_nxt++;
+		}
+	}
+	else if (n > src_len)
+	{
+		j = 0;
+
+		while (src[j] != '\0')
+		{
+			dest[dest_nxt] = src[j];
+			dest_nxt++;
+			j++;
+		}
+		dest[dest_nxt] = '\0';
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,4 +1,5 @@
 
 ** The 0-strcat.c function program concatenates two strings. Returns a pointer to the resulting string dest.
 
+** The 1-strncat.c function program concatenates two strings. it will use at most n bytes from src; and src does not need to be null-terminated if it contains n or more bytes. Return a pointer to the resulting string dest.
 
