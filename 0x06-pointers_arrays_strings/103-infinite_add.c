bfdd67ec8ccd7f0dcac70445dd23cb6cbaf94091
Israel025 <Israeltademola@gmail.com>
2022-04-24 07:47:54 +0000

    Created the 103-infinite_add.c function program file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,15 +1,16 @@
-
 #include "main.h"
 #include <stdio.h>
-/**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
- */
 
+/**
+ * infinite_add - adds two numbers (from strings)
+ * @n1: the first string number.
+ * @n2: the secong string number.
+ * @r: buffer for storing the result.
+ * @size_r: buffer size:
+ *
+ * Description: functions as described above.
+ * Return: r (whatever the value is).
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
