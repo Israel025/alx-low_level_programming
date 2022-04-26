8a01ee0f20417836132a70963fd0f6328900a2af
Israel025 <Israeltademola@gmail.com>
2022-04-26 15:08:13 +0000

    Created the 0-memset.c function program file (fixed)

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte.
- * @s: Memory area pointer.
- * @b: constant byte.
- * @n: Amount of memory bytes
- *
- * Description: functions as described above
- * Return: a pointer to the resulting char s.
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	i = 0;
-	while (i < n)
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
-		i++;
-	}
+		*(s + i) = b; /*add 1 position s*/
+
+	} /*END FOR*/
+
 	return (s);
 }
