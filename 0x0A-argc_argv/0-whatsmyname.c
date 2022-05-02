b64dbae5f3772fea147b503744d708fcdd2b53cd
Israel025 <Israeltademola@gmail.com>
2022-05-02 19:50:10 +0000

    Created the 0-whatsmyname.c program file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name.
+ * @argc: The size of the argument vector array (argv).
+ * @argv: The argument vector array.
+ *
+ * Description: This program does as specified above
+ * and followed by a new line
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,7 @@
+** The 0-whatsmyname.c program file prints its name, followed by a new line.
+
+
+===============ADVANCED TASK================
+============================================
+
+
