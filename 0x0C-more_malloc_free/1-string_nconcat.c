5fb5167b0a8fa0a5eabcc2fb731ed3f6c2075dcb
Israel025 <Israeltademola@gmail.com>
2022-05-09 13:27:56 +0000

    Created the 1-string_nconcat.c function program file

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string.
+ * @s: The string to be examined.
+ *
+ * Description: functions as described above.
+ * Return: integer value of the lenght of string s.
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * string_nconcat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: n string out of s2.
+ *
+ * Description: functions as described above.
+ * Return: the concatenated string (char).
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int i, j, len, lenS2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	lenS2 = _strlen(s2);
+	if (n >= lenS2)
+		n = lenS2;
+
+	len = _strlen(s1);
+	len += n;
+	len += 1;
+
+	concat = malloc(sizeof(char) * len);
+	if (concat == NULL || concat == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		j = 0;
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			concat[j] = s1[i];
+			j++;
+		}
+		for (i = 0; i < n; i++)
+		{
+			concat[j] = s2[i];
+			j++;
+		}
+		concat[j] = s2[i];
+		return (concat);
+	}
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,5 +1,7 @@
 ** The 0-malloc_checked.c funtion program allocates memory using malloc.
 
+** The 1-string_nconcat.c funtion program concatenates two strings.
+
 =================ADVANCED TASKS====================
 ===================================================
 
