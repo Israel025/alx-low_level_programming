3bf9432278c228402f786ec5dda0169562948f89
Israel025 <Israeltademola@gmail.com>
2022-05-09 14:55:58 +0000

    Created the 2-calloc.c function program file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array using malloc.
+ * @nmemb: The number of elements to be allocated.
+ * @size: The size of elements.
+ *
+ * Description: functions as described above.
+ * Return: the address to the allocated space.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *memSpace, i, j, len;
+
+	memSpace = malloc(size * nmemb);
+        if (memSpace == NULL || memSpace == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		i = 0;
+		while (memSpace[i])
+		{
+			memSpace[i] = 0;
+			i++;
+		}
+		return (memSpace);
+	}
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -2,6 +2,8 @@
 
 ** The 1-string_nconcat.c funtion program concatenates two strings.
 
+** The 2-calloc.c funtion program allocates memory for an array, using malloc.
+
 =================ADVANCED TASKS====================
 ===================================================
 
