27f846ccc51fb83c9b7c7f9c6e3153c08ff0607a
Israel025 <Israeltademola@gmail.com>
2022-05-10 17:19:29 +0000

    Created the 101-mul.c funtion program file+

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,7 +4,7 @@
  * main - Multiply two positive numbers.
  * @argc: argument size.
  * @argv: argument vector.
- * 
+ *
  * Description: functions as described above.
  * Return: prints the result.
  */
@@ -25,7 +25,7 @@ int i, j;
 		}
 
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 return (0);
 }
