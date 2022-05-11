10407b1afb31f07a718a6b7df40a4deef3918ff5
Israel025 <Israeltademola@gmail.com>
2022-05-11 12:55:47 +0000

    Created the 2-main.c program file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,32 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - Allocates memory for an array using malloc.
- * @nmemb: The number of elements to be allocated.
- * @size: The size of elements.
+ * main - Prints the name of the file it was compiled from.
  *
  * Description: functions as described above.
- * Return: the address to the allocated space.
+ * followed by a new line.
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+int main(void)
 {
-	char *memSpace;
-	unsigned int i;
-
-	if (nmemb == 0 || size == 0)
-	{
-		return (NULL);
-	}
-
-	memSpace = malloc(size * nmemb);
-	if (memSpace == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < (size * nmemb); i++)
-	{
-		memSpace[i] = 0;
-	}
-	return (memSpace);
+	printf("%s\n", __FILE__);
+	return (0);
 }
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -2,6 +2,8 @@
 
 ** The 1-pi.h header program file defines a macro named PI as an abbreviation for the token 3.14159265359.
 
+** The 2-main.c program file prints the name of the file it was compiled from, followed by a new line.
+
 =================ADVANCED TASKS====================
 ===================================================
 
