a8e1f3fc43997873f85fe56fad982565e9b1d39b
Israel025 <Israeltademola@gmail.com>
2022-05-12 12:57:06 +0000

    Created the 4-new_dog.c function program file +

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,59 @@
 #include "dog.h"
 
 /**
+ * _strlen - returns the lenght of a string.
+ * @s: The string to be examined.
+ *
+ * Description: functions as described above.
+ * Return: integer value of the lenght of string s.
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * str_copy - copies a string from source to dest.
+ * @dest: The destination.
+ * @src: The source.
+ *
+ * Description: functions as described above.
+ * Return: returns dest.
+ */
+char *str_copy(char *dest, char *src)
+{
+	int i, len;
+
+	if (src == NULL)
+	{
+		return (NULL);
+	}
+
+	len = _strlen(src);
+	dest = malloc(sizeof(char) * (len + 1));
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - Creates a new copy of dog struct.
  * @name: The name of the new dog.
  * @age: The age of the new dog.
@@ -14,21 +67,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *bruno;
+	char *aname, *aowner;
 
-	bruno = malloc(sizeof(struct dog));
-
-	if (bruno == NULL)
+	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
-	else
+
+	bruno = malloc(sizeof(dog_t));
+	if (bruno == NULL)
+	{
+		free(bruno);
+		return (NULL);
+	}
+
+	aname = malloc(sizeof(char) * _strlen(name));
+	if (aname == NULL)
+	{
+		free(bruno);
+		return (NULL);
+	}
+	str_copy(aname, name);
+	bruno->name = aname;
+
+	bruno->age = age;
+
+	aowner = malloc(sizeof(char) * _strlen(owner));
+	if (aowner == NULL)
 	{
 		free(bruno->name);
-		free(bruno->owner);
 		free(bruno);
-		bruno->name = name;
-		bruno->age = age;
-		bruno->owner = owner;
+		return (NULL);
 	}
+	str_copy(aowner, owner);
+	bruno->owner = aowner;
+
 	return (bruno);
 }
