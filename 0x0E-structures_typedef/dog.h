fd5844dbe58bb06d062b82cbfa3fdc112758205e
Israel025 <Israeltademola@gmail.com>
2022-05-12 08:43:13 +0000

    Created the dog.h header program file ++

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
 #ifndef DOG_H_INCLUDED
 #define DOG_H_INCLUDED
+
+/**
+ * struct dog - A sample dog struct
+ * @name: The dog's name.
+ * @owner: The dog's owner.
+ * @age: The dog's age.
+ *
+ * Description: As described above
+ */
+
 struct dog
 {
-    char *name;
-    char *owner;
-    float age;
+	char *name;
+	char *owner;
+	float age;
 };
 #endif
