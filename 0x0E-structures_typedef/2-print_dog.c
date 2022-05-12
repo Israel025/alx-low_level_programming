b6880b2ce7dd9f4478d4811840fb0274ed982627
Israel025 <Israeltademola@gmail.com>
2022-05-12 11:07:09 +0000

    Created the 2-print_dog.c function program file++

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -12,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
-		printf("Age: %f\n", (d->age == '\0') ? "(nil)" : d->age);
+		printf("Age: %f\n", (d->age == '\0') ? 0 : d->age);
 		printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
 	}
 }
