c7c91cc047b6a41187f0b0e9e9014a5aa5eb068f
Israel025 <Israeltademola@gmail.com>
2022-05-12 14:26:49 +0000

    Modified the 101-mul.c program file+

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -31,7 +31,6 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		return (!!_puts("Error\n") * 98);
-
 	s1 = argv[1], s2 = argv[2];
 	for (; c1 = s1[len_1], c2 = s2[len_2], c1 || c2; len_1 += !!c1, len_2 += !!c2)
 	{
@@ -39,20 +38,16 @@ int main(int argc, char *argv[])
 		if ((c1 && (c1 < '0' || c1 > '9')) || ((c2 && (c2 < '0' || c2 > '9'))))
 			return (_puts("Error\n"), 98);
 	}
-
 	carry = 0, len_r = len_1 + len_2 + 1;
 	res = malloc(sizeof(char) * len_r--);
 	if (res == NULL)
 		return (_puts("Error\n"), 98);
-
 	len_r_tmp = len_r;
 	while (len_r_tmp > 0)
 		res[len_r_tmp--] = 0;
-
 	len_tmp = len_1, s_tmp = s1;
 	if (len_2 > len_1)
 		s1 = s2, s2 = s_tmp, len_1 = len_2, len_2 = len_tmp;
-
 	for (i = len_2 - 1; i >= 0; i--)
 	{
 		for (j = len_1 - 1; j >= 0; j--)
@@ -64,13 +59,10 @@ int main(int argc, char *argv[])
 		}
 		res[i] += carry, carry = 0;
 	}
-
 	for (i = 0, sig0 = 0; i < len_r; i++, sig0 = sig0 || !!res[i])
 		if (res[i] || sig0 || i == len_r - 1)
 			_putchar(res[i] + '0');
-
 	_putchar('\n');
-
 	free(res);
 	return (0);
 }
