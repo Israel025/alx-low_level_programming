c6b5be15a78937f534103e9459c5cc9e40da985b
Israel025 <Israeltademola@gmail.com>
2022-05-26 19:14:51 +0000

    Modified the 9-insert_nodeint.c function program file 3+

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,33 +2,6 @@
 #include "lists.h"
 
 /**
- * add_nodeint - Adds a new node at the beginning of a linked-list.
- * @head: The head of the singly linked list.
- * @n: Value of the node to be added
- *
- * Description: functions as described above.
- * Return: The address of the new element & NULL if it fails.
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new_top;
-
-	if (head == NULL)
-		return (NULL);
-
-	new_top = malloc(sizeof(listint_t));
-	if (new_top == NULL)
-		return (NULL);
-
-	new_top->n = n;
-	new_top->next = *head;
-
-	*head = new_top;
-
-	return (new_top);
-}
-
-/**
  * insert_nodeint_at_index - Inserts a new node at a
  * given position of linked-list.
  * @head: The head of the singly linked list.
@@ -40,21 +13,26 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *temp;
-	unsigned int len;
+	listint_t *new_node, *temp;
+	unsigned int len = 0;
+
+	if (*head == NULL)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
 	temp = *head;
-	len = 0;
+
+	if (temp == NULL && idx != 0)
+		return (NULL);
 
 	if (idx == 0)
 	{
-		new_node = add_nodeint(head, n);
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 	else
